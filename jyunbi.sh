98fd2d5de9a1fe7443b0a12e5e556dc4a608056f
TANAKA Hidemune <auau@kne.biglobe.ne.jp>
2016-06-05 11:12:15 +0900

    Signed-off-by: TANAKA Hidemune <auau@kne.biglobe.ne.jp>

--- jyunbi.sh
+++ jyunbi.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 /home/user/tool/karaoke/JspInstall.sh
 mkdir /tmp/que
-touch /tmp/que/rireki
+cp rireki /tmp/que/rireki
+echo 70 > /tmp/que/volume
 chmod 777 -R /tmp/que
 chmod 777 /tmp/que/rireki
 chown user:tomcat7 -R /tmp/que
+chown user:tomcat8 -R /tmp/que
 ls -la /tmp/
 
--- tag_mp3.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-java -jar /home/user/tool/karaoke/jtag.jar "$1"
