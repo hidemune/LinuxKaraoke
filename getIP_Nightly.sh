816bf86b05e4e4575e4fe254c52e0c2d485c7972
hidemune <auau@kne.biglobe.ne.jp>
2019-08-21 14:22:53 +0900

    Signed-off-by: hidemune <auau@kne.biglobe.ne.jp>

--- getIP_Nightly.sh
+++ getIP_Nightly.sh
@@ -15,6 +15,13 @@ fi
 str2=`ifconfig | grep "inet 192"`
 ip2=$(echo "$str2" | awk '{print $2}' | sed -n 1P)
 
+if [ "${ip2}" == "" ]; then
+  echo ネットワークアドレスを取得できません。
+  echo WiFiまたはLANケーブルをご確認ください。
+  read a
+  exit 1
+fi
+
 url="http://${ip2}:8080"
 echo 以下のアドレスに、LAN経由で繋いでください。なお、アドレスはクリップボードへコピーされます。
 echo Webブラウザが表示されたら、リンクをどれか選択してください。しばらく待つと、アドレスがペーストされます。
